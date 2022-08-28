e2a4f36de319b3bb7ba9b72a122c4c0b8c6f9ca0
FrancescoXX <francescociullaroma@gmail.com>
2022-08-28 11:38:27 +0200

    project 3 NFT

---
+++ learnweb3_3_NFT/contracts/NFTee.sol
@@ -0,0 +1,14 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+// Import the openzepplin contracts
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+
+// GameItem is  ERC721 signifies that the contract we are creating imports ERC721 and follows ERC721 contract from openzeppelin
+contract GameItem is ERC721 {
+
+    constructor() ERC721("GameItem", "ITM") {
+        // mint an NFT to yourself
+        _mint(msg.sender, 1);
+    }
+}
---
+++ learnweb3_3_NFT/scripts/deploy.js
@@ -0,0 +1,27 @@
+// Import ethers from Hardhat package
+const { ethers } = require("hardhat");
+
+async function main() {
+  /*
+A ContractFactory in ethers.js is an abstraction used to deploy new smart contracts,
+so nftContract here is a factory for instances of our GameItem contract.
+*/
+  const nftContract = await ethers.getContractFactory("GameItem");
+
+  // here we deploy the contract
+  const deployedNFTContract = await nftContract.deploy();
+  
+  // wait for the contract to deploy
+  await deployedNFTContract.deployed();
+
+  // print the address of the deployed contract
+  console.log("NFT Contract Address:", deployedNFTContract.address);
+}
+
+// Call the main function and catch if there is any error
+main()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  });
